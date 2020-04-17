54a96a5a5e23847924cfa8f8bfd3c4776dc11c41
Thomas Wheeler <tbwheeler94@gmail.com>
2020-04-16 22:01:15 -0700

    Fourth Push

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify
 #################################################
 # Measurement Database Setup
 #################################################
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
@@ -59,52 +60,57 @@ def welcome():
         f"/api/v1.0/start/end/<start>/<end><br>"
     )
 
-#Define precipitation page which returns
+#Define precipitation page which returns date and precip value in list of dictionaries
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
+    # Create our session to connect to db
     session = Session(engine)
 
-    # Query all passengers
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-23").all()
+    # Query date and precipitation value for dates after 2016-08-23
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
     session.close()
 
+    # Creates dictionary for each date and precipitation value, puts them into a list
     prcp_list = [] 
     for date, prcp in results:
         date_dict = {date:prcp}
         prcp_list.append(date_dict)
 
+    # Returns jsonified list of precip values
     return jsonify(prcp_list)
 
+#Define stations page which returns array of all station names
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
+    # Create our session to connect to db
     session = Session(engine)
 
-    # Query all passengers
+    # Query station names
     results = session.query(Station.name).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    all_stations = list(np.ravel(results))
 
-    return jsonify(all_names)
+    # Returns jsonified list of all station names
+    return jsonify(all_stations)
 
+#Return last year of temperature data for the most active station
 @app.route("/api/v1.0/tobs")
 def tobs():
-    # Create our session (link) from Python to the DB
+    # Create our session to connect to db
     session = Session(engine)
 
-    # Query all passengers
+    # Query date and temp data and filter for most active station
     results = session.query(Measurement.date, Measurement.tobs)\
                             .filter(Measurement.station == "USC00519281")\
                             .filter(Measurement.date <= "2011-01-01").all()
 
     session.close()
 
-    # Convert list of tuples into normal list
+    # Creates dictionary for each date and temperature value, puts them into a list
     temp_list = [] 
     for date, tobs in results:
         temp_dict = {date:tobs}
@@ -112,6 +118,7 @@ def tobs():
 
     return jsonify(temp_list)
 
+#Define start/end page which provideds TMIN, TMAX, TAVG for dates between provided end and start dates
 @app.route("/api/v1.0/start/end/<start>/<end>")
 def start_end(start, end):
 
@@ -126,6 +133,7 @@ def start_end(start, end):
 
     return jsonify(all_names)
 
+#Define start/end page which provideds TMIN, TMAX, TAVG for dates greater than or equal to provided start date
 @app.route("/api/v1.0/start_only/<start_only>")
 def start_only(start_only):
     
